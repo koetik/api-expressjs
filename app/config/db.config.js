403eab1dc55f5ba9bacbc84ad93d9469291ea9f3
Hasan Su'udi <koetik@gmail.com>
2021-07-07 00:28:22 +0700

    add env package

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "5ur4b4y4",
-    DB: "db_jadwal",
-    PORT: "8889",
+    HOST: process.env.DB_HOST || "localhost",
+    USER: process.env.DB_USER ||"root",
+    PASSWORD: process.env.DB_PASSWORD || "",
+    DB: process.env.DB_NAME || "db_express",
+    PORT: process.env.DB_PORT || "3306",
     dialect: "mysql",
     pool: {
         max: 5,
--- index.js
+++ index.js
@@ -27,6 +27,7 @@ require("./app/routes/micro")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
